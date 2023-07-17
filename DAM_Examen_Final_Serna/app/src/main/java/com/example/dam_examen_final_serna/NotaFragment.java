fc349c7abd4577da6bfaaec8c15af5cb50c76b3c
Juan Carlos Serna Chancolla <2016200215@untels.edu.pe>
2023-07-17 15:49:23 -0500

    Layout Personalizado

--- DAM_Examen_Final_Serna/app/src/main/java/com/example/dam_examen_final_serna/NotaFragment.java
+++ DAM_Examen_Final_Serna/app/src/main/java/com/example/dam_examen_final_serna/NotaFragment.java
@@ -71,9 +71,13 @@ public class NotaFragment extends Fragment {
                 recyclerView.setLayoutManager(new StaggeredGridLayoutManager( mColumnCount, StaggeredGridLayoutManager.VERTICAL));
             }
             notaList = new ArrayList<>();
-            notaList.add(new Nota("UC4","Estudiar para la evaluación de la UC4 - Caso: Notas y Listas", true, android.R.color.holo_blue_light));
-            notaList.add(new Nota("Recordar", "He aparcado el coche en la calle República Argentina, no olvidarme en el parque",false, android.R.color.holo_green_light));
-            notaList.add(new Nota("cumpleaños (fiesta)","no olvidar las velas", true, android.R.color.holo_orange_light));
+            notaList.add(new Nota("Trabajo Academico","Las notas del trabajo académico fueron revisadas e ingresadas el día 16 de Julio.", true, android.R.color.holo_blue_light));
+            notaList.add(new Nota("Examen Final", "Estudiar para el Examen Final: Caso: Notas Listas, es una réplica de la UC4",false, android.R.color.holo_green_light));
+            notaList.add(new Nota("Resultado de Examen Final","La Revisión del examen final se revisarán durante la sesión de clases y el resultado será publicado en los próximos días, especificamente: 24/07/23 - 31/07/23  Examen final ", true, android.R.color.holo_orange_light));
+            notaList.add(new Nota("Examen Sustitutorio","Examen Sustitutorio planificado para el día 24 de julio del 2023. Asistir puntualmente a todos los que se van a presentar.", true,android.R.color.holo_blue_light));
+            notaList.add(new Nota("Fiestas Patrias", "Desfiles militares, ferias gastronómicas y recorridos turísticos se abren para celebrar en este año 2023. ¡Felíz día del Perú!",false, android.R.color.holo_green_light));
+            notaList.add(new Nota("Despedida de Ciclo","Aprecio mucho su amistad, su participación en las sesiones de clases, cada ciclo es una nueva experiencia, deseo que sigan estudiando. Muchas Felicidades. Nos vemos el 2023 - II", true, android.R.color.holo_orange_light));
+            notaList.add(new Nota("Estudia y superate","Todas las distintas capacidades que el hombre posee de la mente, del alma y del cuerpo le fueron dadas por Dios para que las dedique a alcanzar el más alto grado de excelencia posible. Toda facultad y todo atributo con que el Creador nos haya dotado deben emplearse para su gloria y para el ennoblecimiento de nuestros semejantes. Y en este empleo se halla la ocupación más pura, más noble y más feliz.", true, android.R.color.holo_blue_light));
             adapterNotas = new MyNotaRecyclerViewAdapter(notaList,mListener);
             recyclerView.setAdapter(adapterNotas);
         }
