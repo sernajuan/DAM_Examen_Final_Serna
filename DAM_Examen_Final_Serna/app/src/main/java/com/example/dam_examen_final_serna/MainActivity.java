adc57289b407cf596a6cde7cb904b546b21d8276
Juan Carlos Serna Chancolla <2016200215@untels.edu.pe>
2023-07-17 15:24:12 -0500

    Notas en columnas

--- DAM_Examen_Final_Serna/app/src/main/java/com/example/dam_examen_final_serna/MainActivity.java
+++ DAM_Examen_Final_Serna/app/src/main/java/com/example/dam_examen_final_serna/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.dam_examen_final_serna;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Definir el evento click sobre el botón Login
+        btnLogin = findViewById(R.id.buttonLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NotasActivity.class);
+                startActivity(i);
+            }
+
+        });
+
     }
 }
